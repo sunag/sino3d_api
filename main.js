787874ca3369255043c99df59755941363d1354b
sunag <sunagbrasil@gmail.com>
2022-03-27 07:30:57 -0300

    API interior

--- main.js
+++ main.js
@@ -27,10 +27,64 @@ sino3d.addEventListener('complete', () => {
 
 });
 
+const goToExterior = () => {
+	
+	// fade para efeito de transição
+	sino3d.fadeState({
+		fadeOpacity: 1
+	}, 1000, () => {
+		
+		// remove interior da memoria de video
+		sino3d.removeInside();
+		
+		// muda para visão exterior
+		sino3d.setView( 'outside' );
+		
+		// trasição da camera para o ângulo de origem
+		sino3d.fadeCameraToInitialState();
+		
+		// trasição para a exposição padrão
+		sino3d.fadeState({
+			exposure: 1
+		}, 1000 );
+		
+		console.log( 'DONE OUTSIDE!' );
+		
+	} );
+	
+}
+
+const goToInterior = () => {
+	
+	// fade para efeito de transição
+	sino3d.fadeState({
+		exposure: 0
+	}, 1000, () => {
+		
+		// carrega asset ( generico nesse momento )
+		sino3d.openInside( 'master' );
+		
+		console.log( 'LOADING INSIDE' );
+		
+	} );
+
+}
+
+// interior carregado 
+sino3d.addEventListener('insideEnd', () => {
+	
+	// reset camera do interior para o estado inicial
+	sino3d.resetInsideCamera();
+	
+	// muda para visão interior
+	sino3d.setView( 'inside' );
+	
+});
+
 // precione 1 até 6 para alterar as versões
 document.body.onkeydown = ( e ) => {
 
-	const key = e.key;
+	const key = e.key.toLowerCase();
 	
 		 if ( key == 1 ) sino3d.setCarVersion( 'Master Furgão L1H1' );
 	else if ( key == 2 ) sino3d.setCarVersion( 'Master Grand Furgão L2H2' );
@@ -38,5 +92,7 @@ document.body.onkeydown = ( e ) => {
 	else if ( key == 4 ) sino3d.setCarVersion( 'Master Extra Vitré L3H2' );
 	else if ( key == 5 ) sino3d.setCarVersion( 'Master Chassi Cabine L2H1' );
 	else if ( key == 6 ) sino3d.setCarVersion( 'Master Minibus Executive L3H2' );
+	else if ( key == 'i' ) goToInterior();
+	else if ( key == 'e' ) goToExterior();
 
 }
